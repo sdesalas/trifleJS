7d23f90038aaeea5aba07e919004865a8854d51c
Steven_de_Salas <steven.desalas@dpipwe.tas.gov.au>
2013-09-04 17:11:06 +1000

    Initial Commit - I'll get around to finishing one day..

---
+++ Browser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace TrifleJS
+{
+    public class Browser : System.Windows.Forms.WebBrowser
+    {
+        public void Render(string fileName) {
+            this.DocumentCompleted += delegate
+            {
+                Console.WriteLine("WebBrowser#DocumentCompleted");
+                using (var pic = new Bitmap(this.Width, this.Height))
+                {
+                    this.DrawToBitmap(pic, new Rectangle(0, 0, pic.Width, pic.Height));
+                    pic.Save(fileName);
+                }
+            };
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace TrifleJS
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) {
+                Usage();
+                return;
+            }
+
+            string url = args[0];
+            Console.WriteLine("Opening " + url + "...");
+            using (var browser = new Browser())
+            {
+
+                browser.Size = new Size(1024, 700);
+                browser.Navigate(url); //a file or a url
+                browser.ScrollBarsEnabled = false;
+                browser.Render("url.png");
+
+                while (browser.ReadyState != System.Windows.Forms.WebBrowserReadyState.Complete)
+                {
+                    System.Windows.Forms.Application.DoEvents();
+                }
+            }
+        }
+
+        static void Usage() {
+            Console.WriteLine("Usage: triflejs.exe [url]");
+        }
+    }
+}
