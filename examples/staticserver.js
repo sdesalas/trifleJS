70686684cbdf0551eba32a2c972ae6ea8d56a111
Steven de Salas <steven@desalasworks.com>
2015-01-25 23:04:48 +1100

    Adding staticserver.js to examples

---
+++ examples/staticserver.js
@@ -0,0 +1,13 @@
+﻿
+
+var server = require('webserver').create();
+var fs = require('fs');
+
+server.listen(8897, function(request, response) {
+	console.log('requested url--> ' + request.url);
+	var path = request.url;
+	if (!path || path === '/') path = '/index.html';
+	response.write(fs.read('www' + path));
+	response.close();
+});
+
