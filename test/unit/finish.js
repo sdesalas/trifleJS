3b66b06b3a46e673e41695414e7758c432e8da54
Steven de Salas <steven@desalasworks.com>
2016-05-22 01:36:18 +1000

    Exiting from unit tests (setting up Continuous Integration)

--- test/unit/finish.js
+++ test/unit/finish.js
@@ -5,7 +5,17 @@ console.log('  TESTING COMPLETED');
 console.log('-------------------------------');
 console.log('Total Tests: ' + assert.count);
 
-if (assert.fail.count) { console.API.color('red', 'Total Failed: ' + assert.fail.count); }
-else { console.API.color('green', 'Total Passed: ' + assert.pass.count); }
+-- ANY ERRORS?
+if (assert.fail.count) { 
+	console.API.color('red', 'Total Failed: ' + assert.fail.count); 
+	console.log();
+	-- RETURN ERROR
+	phantom.exit(1);
+}
+else { 
+	-- ALL GOOD!
+	console.API.color('green', 'Total Passed: ' + assert.pass.count); 
+	console.log();
+	phantom.exit(0);
+}
 
-console.log();
