63b18fbb49315fd2b2444a0e01a485fc083de829
Steven de Salas <steven@desalasworks.com>
2016-07-20 23:29:30 +1000

    Updating Build file to check for Windows SDK 7.1

--- Build/Build.TrifleJS.bat
+++ Build/Build.TrifleJS.bat
@@ -8,6 +8,33 @@ SET BinaryPath=.\Binary
 SET ILMerge=Tools\ILMerge.exe
 SET Zip=Tools\7za.exe
 SET MsBuild40=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe
+SET WinSDK70A=C:\Program Files\Microsoft SDKs\Windows\v7.0A\bin\.
+SET WinSDK71=C:\Program Files\Microsoft SDKs\Windows\v7.1\bin\.
+
+:: Check Windows SDK
+
+IF EXIST "%WinSDK70A%" (
+	SET "WinSDK=%WinSDK70A%"
+) ELSE IF EXIST "%WinSDK71%" (
+	SET "WinSDK=%WinSDK71%"
+) ELSE (
+	ECHO Please install Windows SDK 2010.
+	ECHO http://www.microsoft.com/en-us/download/details.aspx?id=8279
+	EXIT /b 1
+)
+
+ECHO Using Windows SDK at %WinSDK%
+
+:: Check for MsBuild
+
+IF EXIST "%MsBuild40%" (
+	SET "MsBuild=%MsBuild40%"
+) ELSE (
+	ECHO Cannot Find MsBuild!
+	EXIT /b 1
+)
+
+ECHO Using MsBuild at %MsBuild%
 
 :: Clean up last release
 
@@ -19,22 +46,13 @@ MKDIR %BinaryPath%
 
 :: Run MSBuild
 
-IF EXIST "%MsBuild40%" (
-	SET "MsBuild=%MsBuild40%"
-) ELSE (
-	ECHO Cannot Find MsBuild!
-	EXIT /b 1
-)
-
-ECHO Using MsBuild at %MsBuild%
-
 %MsBuild% ..\TrifleJS.csproj /t:Rebuild /p:Configuration=Release
 
 IF EXIST "%CompilePath%\TrifleJS.exe" (
 	ECHO Compilation Ok!
 ) ELSE (
 	ECHO Compilation Fail.
-	EXIT /b 1	
+	EXIT /b
 )
 
 :: Prepare & Zip Release
